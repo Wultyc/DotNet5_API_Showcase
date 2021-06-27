cac8f9b9ef7aff3d76c5964609d49a45481e4b90
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 11:52:02 +0100

    Users API Controller base

---
+++ DotNet5_API_Showcase/Controllers/UsersController.cs
@@ -0,0 +1,23 @@
+﻿using DotNet5_API_Showcase.Models;
+using DotNet5_API_Showcase.Repositories.Interface;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_API_Showcase.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersController : Controller
+    {
+
+        private IUserRepository userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+    }
+}
