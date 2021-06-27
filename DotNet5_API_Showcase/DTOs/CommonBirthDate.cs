fbe742388d017bb2adb434a3c7dff2598b903bbc
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 14:33:43 +0100

    DTO birthDate for common use on others DTOs

---
+++ DotNet5_API_Showcase/DTOs/CommonBirthDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_API_Showcase.DTOs
+{
+    public record CommonBirthDate
+    {
+        [Required]
+        public int day { get; init; }
+        [Required]
+        public int month { get; init; }
+        [Required]
+        public int year { get; init; }
+    }
+    
+}
