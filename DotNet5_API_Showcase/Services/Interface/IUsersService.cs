8aeffbbf44711a5a7f87f97317504383af5b8bb1
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 15:50:26 +0100

    Add Service Class and Interface for Users

---
+++ DotNet5_API_Showcase/Services/Interface/IUsersService.cs
@@ -0,0 +1,21 @@
+﻿using DotNet5_API_Showcase.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_API_Showcase.Interface.Services
+{
+    public interface IUsersService
+    {
+        public Task<IEnumerable<User>> GetUsers();
+
+        public Task<User> CreateUser(User user);
+
+        public Task<User> GetUserById(Guid userId);
+
+        public Task<User> UpdateUser(User user);
+
+        public Task DeleteUser(Guid userId);
+    }
+}
---
+++ DotNet5_API_Showcase/Services/UsersService.cs
@@ -0,0 +1,45 @@
+﻿using DotNet5_API_Showcase.Interface.Services;
+using DotNet5_API_Showcase.Models;
+using DotNet5_API_Showcase.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_API_Showcase.Services
+{
+    public class UsersService : IUsersService
+    {
+        private IUserRepository userRepository;
+
+        public UsersService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            return await this.userRepository.CreateUser(user);
+        }
+
+        public async Task DeleteUser(Guid userId)
+        {
+            await this.userRepository.DeleteUser(userId);
+        }
+
+        public async Task<User> GetUserById(Guid userId)
+        {
+            return await this.userRepository.GetUserById(userId);
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            return await this.userRepository.GetUsers();
+        }
+
+        public async Task<User> UpdateUser(User user)
+        {
+            return await this.userRepository.UpdateUser(user);
+        }
+    }
+}
