db7d9af0155ce878b3d0451f9b95324ada3f5256
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 14:04:59 +0100

    Make UserResponse props mandatory

--- DotNet5_API_Showcase/DTOs/UserResponse.cs
+++ DotNet5_API_Showcase/DTOs/UserResponse.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,9 +8,13 @@ namespace DotNet5_API_Showcase.DTOs
 {
     public record UserResponse
     {
+        [Required]
         public Guid userId { get; init; }
+        [Required]
         public string name { get; init; }
+        [Required]
         public string birthDate { get; init; }
+        [Required]
         public string email { get; init; }
     }
 }
