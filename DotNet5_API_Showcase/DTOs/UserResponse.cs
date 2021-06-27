271015f9f790c82909dc88ec88bb5e2cd7227fe9
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 13:48:20 +0100

    DTO for response with User model

---
+++ DotNet5_API_Showcase/DTOs/UserResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_API_Showcase.DTOs
+{
+    public record UserResponse
+    {
+        public Guid userId { get; init; }
+        public string name { get; init; }
+        public string birthDate { get; init; }
+        public string email { get; init; }
+    }
+}
