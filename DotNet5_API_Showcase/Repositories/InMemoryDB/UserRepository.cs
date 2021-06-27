2babd3183780c321beef9d181e8145032e7172cb
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 11:52:53 +0100

    Users In Memory Repository base

---
+++ DotNet5_API_Showcase/Repositories/InMemoryDB/UserRepository.cs
@@ -0,0 +1,45 @@
+﻿using DotNet5_API_Showcase.Models;
+using DotNet5_API_Showcase.Repositories.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5_API_Showcase.Repositories.InMemoryDB
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly List<User> userList = new()
+        {
+            new User { userId = Guid.NewGuid(), name = "Jonh", birthDate = new DateTime(1974, 06, 24, 0, 0, 0, DateTimeKind.Local), email = "john@example.com" },
+            new User { userId = Guid.NewGuid(), name = "George", birthDate = new DateTime(1997, 05, 12, 0, 0, 0, DateTimeKind.Local), email = "george@example.com" },
+            new User { userId = Guid.NewGuid(), name = "Michel", birthDate = new DateTime(1984, 09, 03, 0, 0, 0, DateTimeKind.Local), email = "michel@example.com" },
+            new User { userId = Guid.NewGuid(), name = "Bella", birthDate = new DateTime(1996, 08, 30, 0, 0, 0, DateTimeKind.Local), email = "bella@example.com" }
+        };
+
+        public async Task<User> CreateUser()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task DeleteUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> GetUserById(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> UpdateUser(Guid userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
