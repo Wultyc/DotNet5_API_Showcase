18d4fd3304283420cc39ac4ddcdfdd6995b7854e
Jorge Gabriel Azevedo <jorge.gfm.azevedo@outlook.pt>
2021-06-27 11:32:06 +0100

    Repository interface

---
+++ DotNet5_API_Showcase/Repositories/Interface/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNet5_API_Showcase.Models;
+
+namespace DotNet5_API_Showcase.Repositories.Interface
+{
+    interface IUserRepository
+    {
+        public Task<IEnumerable<User>> GetUsers();
+        public Task<User> GetUserById(Guid userId);
+        public Task<User> CreateUser();
+        public Task<User> UpdateUser(Guid userId);
+        public Task DeleteUser(Guid userId);
+    }
+}
